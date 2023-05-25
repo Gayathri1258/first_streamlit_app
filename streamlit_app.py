7df344ebf3ddfc33b7144d4be2b9f77526e0bb50
Gayathri1258 <133978598+Gayathri1258@users.noreply.github.com>
2023-05-25 17:57:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,14 +36,13 @@ try:
 except URLError as e:
   streamlit.error()
 
-
-
 def get_fruityvice_data(this_fruit_choice):
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       return fruityvice_normalized
    
-   streamlit.stop()
+streamlit.stop()
+
 #import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
